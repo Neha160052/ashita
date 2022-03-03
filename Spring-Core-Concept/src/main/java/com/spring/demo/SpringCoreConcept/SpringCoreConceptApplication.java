106bbb7c97c529376170a0950dd381843346854e
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-04 00:21:14 +0530

    application context

--- Spring-Core-Concept/src/main/java/com/spring/demo/SpringCoreConcept/SpringCoreConceptApplication.java
+++ Spring-Core-Concept/src/main/java/com/spring/demo/SpringCoreConcept/SpringCoreConceptApplication.java
@@ -2,17 +2,19 @@ package com.spring.demo.SpringCoreConcept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreConceptApplication {
 
 	public static void main(String[] args) {
 
-		BinarySearchImpl binarysearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		// BinarySearchImpl binarysearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		ApplicationContext applicationContext = SpringApplication.run(SpringCoreConceptApplication.class, args);
+		BinarySearchImpl binarysearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarysearch.binarySearch(new int[] {12,4,6},3);
 		System.out.println(result);
 
-		SpringApplication.run(SpringCoreConceptApplication.class, args);
 	}
 
 }
