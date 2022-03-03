caae9b782b3f933abd34ec00c63d1fde535332a4
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-03 23:52:38 +0530

    Spring core concept

---
+++ Spring-Core-Concept/src/main/java/com/spring/demo/SpringCoreConcept/SpringCoreConceptApplication.java
@@ -0,0 +1,13 @@
+package com.spring.demo.SpringCoreConcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreConceptApplication.class, args);
+	}
+
+}
---
+++ Spring-Core-Concept/src/test/java/com/spring/demo/SpringCoreConcept/SpringCoreConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.demo.SpringCoreConcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
