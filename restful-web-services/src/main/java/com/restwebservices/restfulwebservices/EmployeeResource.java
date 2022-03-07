135adee1ad70492849f7de31b31363242f13e07f
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-08 00:16:55 +0530

    using path variable top get one employee

--- restful-web-services/src/main/java/com/restwebservices/restfulwebservices/EmployeeResource.java
+++ restful-web-services/src/main/java/com/restwebservices/restfulwebservices/EmployeeResource.java
@@ -3,6 +3,7 @@ package com.restwebservices.restfulwebservices;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,4 +18,10 @@ public class EmployeeResource {
     public List<Employee> retrieveAllEmployee() {
         return service.findAll();
     }
+
+    @GetMapping("/employees/{id}")
+    public Employee retrieveEmployee(@PathVariable int id){
+        return service.findOne(id);
+    }
+
 }
