0e095e3a57e4f3b1991b20a1aba463b501c3585d
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-08 00:58:19 +0530

    Exception Handling for resource not found

--- restful-web-services/src/main/java/com/restwebservices/restfulwebservices/EmployeeResource.java
+++ restful-web-services/src/main/java/com/restwebservices/restfulwebservices/EmployeeResource.java
@@ -2,8 +2,11 @@ package com.restwebservices.restfulwebservices;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,11 +22,17 @@ public class EmployeeResource {
 
     @GetMapping("/employees/{id}")
     public Employee retrieveEmployee(@PathVariable int id){
-        return service.findOne(id);
+        Employee employee = service.findOne(id);
+        if (employee == null)
+            throw new RuntimeException("id-"+id);
+        return employee;
     }
 
     @PostMapping("/employees")
-    public void createEmployee(@RequestBody Employee employee){
+    public ResponseEntity<Object> createEmployee(@RequestBody Employee employee){
         Employee savedEmployee = service.save(employee);
+
+       URI location =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedEmployee.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
