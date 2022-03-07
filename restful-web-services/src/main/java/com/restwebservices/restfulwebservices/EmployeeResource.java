8e135e2f62573f0ccfc2137ff91454267d9db1f5
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-08 00:12:48 +0530

    get list of employees

--- restful-web-services/src/main/java/com/restwebservices/restfulwebservices/EmployeeResource.java
+++ restful-web-services/src/main/java/com/restwebservices/restfulwebservices/EmployeeResource.java
@@ -1,5 +1,7 @@
 package com.restwebservices.restfulwebservices;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,11 @@ import java.util.List;
 @RestController
 public class EmployeeResource {
 
+    @Autowired
     private EmployeeDaoService service;
 
     @GetMapping("/employees")
-    public List<EmployeeDaoService> retrieveAllEmployee(){
+    public List<Employee> retrieveAllEmployee() {
         return service.findAll();
     }
 }
