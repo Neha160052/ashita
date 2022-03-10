c29f74b7ec9790a3d183511bbd92f4d5711ef5d7
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-10 22:59:59 +0530

    Spring Data JPA with Hibernate Part 1

---
+++ Spring-Data-JPA-with-Hibernate/src/main/java/com/SpringDataJPA/SpringDataJPAwithHibernate/SpringDataJpaWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.SpringDataJPA.SpringDataJPAwithHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWithHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaWithHibernateApplication.class, args);
+	}
+
+}
---
+++ Spring-Data-JPA-with-Hibernate/src/test/java/com/SpringDataJPA/SpringDataJPAwithHibernate/SpringDataJpaWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringDataJPA.SpringDataJPAwithHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaWithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
