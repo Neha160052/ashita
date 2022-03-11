8a9a10e36b63f5ce98610e38ef7c0a35d21764cd
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-11 21:58:10 +0530

    JPQL

---
+++ Spring-Data-JPA-with-Hibernate-Part2/src/main/java/com/SpringDataJPAwithHibernate/SpringDataJPAwithHibernatePart2/SpringDataJpaWithHibernatePart2Application.java
@@ -0,0 +1,13 @@
+package com.SpringDataJPAwithHibernate.SpringDataJPAwithHibernatePart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWithHibernatePart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaWithHibernatePart2Application.class, args);
+	}
+
+}
---
+++ Spring-Data-JPA-with-Hibernate-Part2/src/test/java/com/SpringDataJPAwithHibernate/SpringDataJPAwithHibernatePart2/SpringDataJpaWithHibernatePart2ApplicationTests.java
@@ -0,0 +1,71 @@
+package com.SpringDataJPAwithHibernate.SpringDataJPAwithHibernatePart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@SpringBootTest
+class SpringDataJpaWithHibernatePart2ApplicationTests {
+
+	@Autowired
+	StudentRepository repository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testStudentCreate() {
+		Student student = new Student();
+		student.setFirstName("John");
+		student.setLastName("Daniel");
+		student.setScore(88);
+
+		Student student2 = new Student();
+		student2.setFirstName("Bill");
+		student2.setLastName("Gates");
+		student2.setScore(98);
+
+		repository.save(student);
+		repository.save(student2);
+	}
+
+	@Test
+	public void testFindAllStudents() {
+		System.out.println(repository.findAllStudents(PageRequest.of(0, 5, Sort.Direction.DESC, "id")));
+	}
+
+	@Test
+	public void testFindAllStudentsPartial() {
+		List<Object[]> partialData = repository.findAllStudentsPartialData();
+		for (Object[] objects : partialData) {
+			System.out.println(objects[0]);
+			System.out.println(objects[1]);
+		}
+	}
+
+	@Test
+	public void testFindAllStudentsByFirstName() {
+		System.out.println(repository.findAllStudentsByFirstName("Bill"));
+	}
+
+
+	@Test
+	public void testFindAllStudentsByScores() {
+		System.out.println(repository.findStudentsForGivenScores(80, 90));
+	}
+
+	@Test
+	@Transactional
+	@Rollback(false)
+	public void testDeleteStudentsByFirstName() {
+		repository.deleteStudentsByFirstName("Bill");
+	}
+
+}
