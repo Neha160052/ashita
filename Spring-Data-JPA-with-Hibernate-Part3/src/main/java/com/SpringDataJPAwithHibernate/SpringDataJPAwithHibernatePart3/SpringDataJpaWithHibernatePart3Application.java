36af2a74eb6109e7c26c427587d3ae5b1de3bac1
Ashita Gupta <ashita.gupta@tothenew.com>
2022-03-14 21:56:31 +0530

    Spring-Data-JPA-with-Hibernate-Part3

---
+++ Spring-Data-JPA-with-Hibernate-Part3/src/main/java/com/SpringDataJPAwithHibernate/SpringDataJPAwithHibernatePart3/SpringDataJpaWithHibernatePart3Application.java
@@ -0,0 +1,13 @@
+package com.SpringDataJPAwithHibernate.SpringDataJPAwithHibernatePart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWithHibernatePart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaWithHibernatePart3Application.class, args);
+	}
+
+}
